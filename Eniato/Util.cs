dbbb79672eb6f4c773425141f93b021b50673801
GabrielRStabile <gabrielrostabile@outlook.com>
2020-12-26 10:30:33 -0300

    :art: Classe Util criada para reunir métodos que podem ser reutilizados em outras telas

---
+++ Eniato/Util.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eniato
+{
+    class Util
+    {
+        public static void Moeda(ref TextBox txt)
+        {
+            string n = string.Empty;
+            double v = 0;
+            try
+            {
+                n = txt.Text.Replace(",", "").Replace(".", "");
+                if (n.Equals(""))
+                    n = "";
+                n = n.PadLeft(3, '0');
+                if (n.Length > 3 && n.Substring(0, 1) == "0")
+                    n = n.Substring(1, n.Length - 1);
+                v = Convert.ToDouble(n) / 100;
+                txt.Text = string.Format("{0:N}", v);
+                txt.SelectionStart = txt.Text.Length;
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro: " + erro);
+            }
+        }
+        public static void SoNumeros(ref TextBox txt, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+            (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+    }
+}
